8c0ca1084c790569c697c335f994b63a49871bcc
Leonardo <leonardo.fc2@outlook.com>
2019-08-12 18:27:46 -0300

    fix: removed mock of model

--- src/controllers/PaisController.js
+++ src/controllers/PaisController.js
@@ -6,14 +6,55 @@ module.exports={
         return response.json({ok:"abc"})    
     },
     async store(require,response){
-        const {Pais,Comida,Bebida} = require.body
-        console.log(Pais,Comida,Bebida)
+        const {
+                Name,
+                Sufficient_Food,
+                Sufficient_to_Drink,
+                Safe_Sanitation,
+                Education,
+                Healthy_Life,
+                Gender_Equality,
+                Income_Distribution,
+                Population_Growth,
+                Good_Governance,
+                Biodiversity,
+                Renewable_Water_Resources,
+                Consumption,
+                Energy_Use,
+                Energy_Savings,
+                Greenhouse_Gases,
+                Renewable_Energy,
+                Organic_Farming,
+                Genuine_Savings,
+                GDP,
+                Employment,
+                Public_Debt
+        } = require.body
+        console.log(Name)
         const pais =  await PaisModel.create({
-            Pais,
-            Bebida,
-            Comida
+                Name,
+                Sufficient_Food,
+                Sufficient_to_Drink,
+                Safe_Sanitation,
+                Education,
+                Healthy_Life,
+                Gender_Equality,
+                Income_Distribution,
+                Population_Growth,
+                Good_Governance,
+                Biodiversity,
+                Renewable_Water_Resources,
+                Consumption,
+                Energy_Use,
+                Energy_Savings,
+                Greenhouse_Gases,
+                Renewable_Energy,
+                Organic_Farming,
+                Genuine_Savings,
+                GDP,
+                Employment,
+                Public_Debt
         })
-        console.log(pais)
-        return response.json({ok:`${Pais} foi salvo com sucesso`})
+        return response.json({ok:`${Name} foi salvo com sucesso`})
     }
 }
