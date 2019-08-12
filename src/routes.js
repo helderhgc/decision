b156e030f2327fb9e3de09338a9a51bbe0d5ef17
Leonardo <leonardo.fc2@outlook.com>
2019-08-12 01:53:21 -0300

    new router for csv import

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const PaisController = require('./controllers/PaisController')
+const CsvController = require('./controllers/CsvController')
 const baseUrl =' http://localhost.com:3333'
 
 const routes = express.Router()
 
 routes.get('/pais',PaisController.index)
 routes.post('/pais',PaisController.store)
+//routes.delete('/csv',CsvController.delete)
+routes.post('/csv',CsvController.store)
 
 
 module.exports= routes
