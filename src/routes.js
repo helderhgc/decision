6e20ee713631795e9ec35bb6285bfb25e3da936b
Leonardo <leonardo.fc2@outlook.com>
2019-08-11 22:05:47 -0300

    feature: post route

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ const baseUrl =' http://localhost.com:3333'
 const routes = express.Router()
 
 routes.get('/pais',PaisController.index)
+routes.post('/pais',PaisController.store)
 
 
 module.exports= routes
