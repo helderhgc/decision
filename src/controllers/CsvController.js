47a0f6b008982dce80e14fc3b1f0882c2fb108f8
Leonardo <leonardo.fc2@outlook.com>
2019-08-12 01:53:34 -0300

    csv controller

---
+++ src/controllers/CsvController.js
@@ -0,0 +1,16 @@
+const converter = require('../util/transformCsvToJson')
+const axios = require('axios')
+module.exports ={
+    async store(req,res){
+        const jsonArray = await converter('/home/leonardo/Documents/Projetos/decision/csv/TestePaises.csv')
+        for(i=0;i<jsonArray.length;i++)
+        {
+            await axios.post('http://localhost:3333/pais',{
+                Pais:jsonArray[i].Pais,
+                Comida:jsonArray[i].Comida,
+                Bebida:jsonArray[i].Bebida
+            })
+        }
+        return res.json({ok:"true"})
+    }
+}
