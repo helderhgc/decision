4dd73e58d219d44e4e667c5829ef7f575e77a803
Leonardo <leonardo.fc2@outlook.com>
2019-08-11 22:38:43 -0300

    feature: store

--- src/controllers/PaisController.js
+++ src/controllers/PaisController.js
@@ -1,12 +1,18 @@
-
+const Pais = require('../models/Pais')
 
 module.exports={
     index(require,response){
         console.log(require.body)
         return response.json({ok:"abc"})
     },
-    store(require,response){
-        console.log(require.body)
-        return response.json({ok:"ok"})
+    async store(require,response){
+        const {name,comida,bebida} = require.body
+        const pais =  await Pais.create({
+            name,
+            bebida,
+            comida,
+        })
+        console.log(pais)
+        return response.json({ok:`${name} foi salvo com sucesso`})
     }
 }
